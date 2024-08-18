42d21fb41a715492b647523e5f03f8bb66ea28e0
Jitesh <jiteshnayak2004@gmail.com>
2024-08-18 20:00:15 +0530

    added cachehit logic and some cache struct mods

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
+/*
+
+current plans: simulate a write through based direct mapped cache
+future plans:
+1. multilevel set associative cache
+2. simulating coherency some sorta
+
+thoughts:
+
+1.while modelling cache fetching from memory wagera is actually not needed
+2.what we intend to do is say there's a miss that entry we make it such that
+we make note of misses and hits and update just the tag field data isn't 
+relevant
+*/
+
+#include <bitset>
+#include <cstdlib>
+#include <ostream>
+#include <string>
 #include <vector>
 #include <cmath>
 #include <stdio.h>
@@ -9,12 +28,16 @@ using namespace std;
 
 // structure of a write back cache line
 struct cache_entry {
-	vector<bool> valid;
-	vector<bool> dirty;
+	bool valid;
+	bool dirty;
 	vector<bool> tag;
 	vector<bool> data;
 };
 
+struct memory_entry {
+    bitset<32> address;
+    int value;
+};
 
 // Function to initialize cache entries with random values
 void initialize_cache(cache_entry *cache, int cache_index, int cache_tag, int cache_block_size) {
@@ -22,8 +45,8 @@ void initialize_cache(cache_entry *cache, int cache_index, int cache_tag, int ca
 
     for (int i = 0; i < cache_index; i++) {
         // Initialize valid and dirty bits with random true/false
-        cache[i].valid.push_back(rand() % 2);
-        cache[i].dirty.push_back(rand() % 2);
+        cache[i].valid=rand()%2;
+        cache[i].dirty=rand()%2;
 
         // Initialize tag with random bits
         for (int j = 0; j < cache_tag; j++) {
@@ -43,7 +66,7 @@ void print_cache_contents(cache_entry *cache, int cache_index, int cache_tag, in
     cout << "---------------------------------------------------------------------\n";
 
     for (int i = 0; i < cache_index; i++) {
-        cout << i << "     | " << cache[i].valid[0] << "     | " << cache[i].dirty[0] << "     | ";
+        cout << i << "     | " << cache[i].valid << "     | " << cache[i].dirty << "     | ";
 
         // Print tag
         for (int j = 0; j < cache_tag; j++) {
@@ -59,6 +82,53 @@ void print_cache_contents(cache_entry *cache, int cache_index, int cache_tag, in
     }
 }
 
+bool IsCacheHit(cache_entry  *cache, string  address,int cache_lines) {
+
+
+    bitset<32> bit_address;
+    int decimal_address;
+
+    // bits to encode no of cache lines
+    int bits_cache_lines=ceil(log2(cache_lines));
+    //converting the hex addr in trace to bits
+    decimal_address=stoi(address, nullptr, 16);
+
+    //finding index the mem_address corresponds to in the cache
+    int address_index=decimal_address%cache_lines;
+
+    cout<<"the address maps to this index in cache "<<(address_index)<<endl; 
+    cout<<"the decimal equivalent of addr is "<<decimal_address<<endl;
+
+    // address in 0s and 1s
+    std::bitset<32> binary_address(decimal_address);
+   
+
+    // extracting the tag bits from msb to lsb from address
+    vector<bool> address_tag;
+    for(int i=31;i<bits_cache_lines;i--) {
+
+	address_tag.push_back(binary_address[i]);
+    }
+
+    // checking valid and tag fields
+    if(cache[address_index].valid==1) {
+	
+	if(cache[address_index].tag==address_tag) {
+
+	    return 1;
+	}
+
+	else {
+	    return 0;
+	}
+
+    }
+
+    else{
+	return 0;
+    }
+
+}
 
 int main() {
 
@@ -95,9 +165,9 @@ int main() {
 
 
 	cout<<"no of cache blocks are "<<cache_blocks<<endl;
-	cout<<"cache block size is "<<cache_block_size<<endl;
-	cout<<"the tag is "<<cache_tag<<endl;
-	cout<<"the index is "<<cache_index<<endl;
+	cout<<"cache block size is "<<cache_block_size<< "bytes"<<endl;
+	cout<<"the tag is "<<cache_tag<<" bits long"<<endl;
+	cout<<"the index is "<<cache_index<<" bits long"<<endl;
 
 	
 	// resizing the struct's data and tag elements
@@ -113,20 +183,31 @@ int main() {
 	
 	//printing the modified sizes
 	
-	for(int i=0;i<cache_index;i++) {
-	
-
-		cout<<"the size of tag is "<<size(cache[i].tag)<<endl;
-		cout<<"the size of data in cache is "<<size(cache[i].data)<<endl;
-
-	}
-
+	// for(int i=0;i<cache_index;i++) {
+	// 
+	//
+	// 	cout<<"the size of tag is "<<size(cache[i].tag)<<endl;
+	// 	cout<<"the size of data in cache is "<<size(cache[i].data)<<endl;
+	//
+	// }
+	//
 
 	// Initialize cache with random values
 	initialize_cache(cache, cache_index, cache_tag, cache_block_size);
 
 	// Print the modified sizes and contents
 	print_cache_contents(cache, cache_index, cache_tag, cache_block_size);
+
+
+	
+	IsCacheHit(cache,"f" ,10);
+
+
+
+
+
+
+
 
 	return 0;
 
