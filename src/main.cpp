24b53ba2cb71aaf0da53c72ff2d1ea082da5f23c
Jitesh <pes2202101201@pesu.pes.edu>
2025-03-04 01:14:04 +0530

    some random commit message

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,6 @@ thoughts:
 1.while modelling cache fetching from memory wagera is actually not needed
 2.what we intend to do is say there's a miss that entry we make it such that
 we make note of misses and hits and update just the tag field data isn't 
-relevant
-hello
-hello
-reandom
-ksjhgf
 
 tasks:
 1. convert direct map as a special case of set associative
